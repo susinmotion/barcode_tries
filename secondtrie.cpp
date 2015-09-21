ee003d5f4731ac9a81587d4740c11d1880630635
SusInMotion <steinman.tutoring@gmail.com>
2015-09-21 13:59:27 -0400

    formatting

--- secondtrie.cpp
+++ secondtrie.cpp
@@ -8,80 +8,63 @@
 using namespace std;
 
 void read_file_into_trie(Trie* trie){
-	string fname = "shorter.fastq";
-	//string hname = "hist.txt";
-//	string gene = "rpob";
-int count=0;    
-//	string aligncheck = "TTCGGTTCCAGCCAGC";
-//	int alignshift = 5;
-	ifstream readfile (fname.c_str());
+	string filename = "shorter.fastq";
+    int count=0;    
+	ifstream readfile (filename.c_str());
 
-
-    	string barcode;
+    string barcode;
 	int throwout;
 	string throwoutstring;
-	if (readfile.is_open())
-{ cout<<"file isopen"<<endl;
-getline(readfile,barcode);
- 	   while (readfile>>throwout)
-    		{
-		count++;
-		readfile>>barcode;
-		if (barcode.find("N")==-1){
+
+//  string hname = "hist.txt";
+//  string gene = "rpob";
+//  string aligncheck = "TTCGGTTCCAGCCAGC";
+//  int alignshift = 5;
 	
-	/*	istringstream iss(line);
-        	int throwthisout;
-        	string sequence;
-        	if (!(iss >> sequence>> throwthisout)){ break; }
-*/
-      		trie->addBarcode(barcode);
-		}
-readfile>>throwoutstring;
-    	
-		
-	}
+    if (readfile.is_open()){ 
+        cout<<"file isopen"<<endl;
+        getline(readfile,barcode);
+        while (readfile>>throwout){
+            count++;
+            readfile>>barcode;
+            if (barcode.find("N")==-1){
+          		trie->addBarcode(barcode);
+            }
+            readfile>>throwoutstring;
+    	}
+    }
 }
-}
 
-int return_max_count(Node* current, int &max )
-{
-
-        vector <Node*> children = current->children();
-        if (!children.empty()){
-
-                for (int i=0; i<children.size(); i++){
-
-                        current = children[i];
-                        return_max_count(current, max);
-                }
+int return_max_count(Node* current, int &max ){
+    vector <Node*> children = current->children();
+    
+    if (!children.empty()){
+        for (int i=0; i<children.size(); i++){
+            current = children[i];
+            return_max_count(current, max);
         }
-        else if((current->count()!=0) && (current->count()>max) ){
-	max=current->count();
-	return max;
+    } 
+    else if((current->count()!=0) && (current->count()>max) ){
+        max=current->count();
+        return max;
+    }   
+    return max;
 }
-return max;
-}
 
-
-
-
-void print_trie(Node* current, string outputString=string(1000,'\0'), int index = 0)
-{
-        outputString[index] = current->content();
-        vector <Node*> children = current->children();
-        if (!children.empty()){
-
-                for (int i=0; i<children.size(); i++){
-
-                        current = children[i];
+void print_trie(Node* current, string barcode, int index){
+    barcode[index] = current->content();
+    vector <Node*> children = current->children();
+    if (!children.empty()){
+        for (int i=0; i<children.size(); i++){
+            current = children[i];
 			index++;
-                        print_trie(current, outputString, index);
-                }
+            print_trie(current, barcode, index);
         }
+    }
 	else if(current->count()!=0){
-cout<<"Barcode "<<outputString<<" was found "<<current->count()<<" times."<<endl;
-return;
-}
+        cout<<"Barcode "<<barcode<<" was found "<<current->count()<<" times."<<endl;
+        return;
+    }
 }
 
 int main()
@@ -99,20 +82,20 @@ int main()
     trie->outputBarcodeCount("Hello");
     trie->outputBarcodeCount("Hellow");    
 
+    Node* current = trie->root();
+    cout<<endl<<"printing the trie"<<endl;
+    print_trie(current, string(1000,'\0'), 0);
+
     Trie* trie2 = new Trie();
     trie2->outputBarcodeCount("testing");
-
-Node* current = trie->root();
-    cout<<"printing the trie"<<endl<<endl;
-    print_trie(current);
+    //output barcode count and print trie don't work until something has been put in the trie.
 
     read_file_into_trie(trie2);
     current= trie2->root();
-    // print_trie(current);
-int max=0;
-max=return_max_count(current, max);
-cout<<max<<" is the max."<<endl;    
-cout << "I'm done "<< endl;
+    int max=0;
+    max=return_max_count(current, max);
+    cout<<max<<" is the max."<<endl;    
+    cout << "I'm done "<< endl;
     delete trie;
 }
 
