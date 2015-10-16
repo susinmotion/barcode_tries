34b6b8b1adb75bf7135047cd6ae02f42f0abf0e3
SusInMotion <steinman.tutoring@gmail.com>
2015-10-16 17:19:17 -0400

    more major refactor. PROBLEM--eg. W/S/S, where the Ss are at the same position. first s gets edited. it doesn't match the second s, so it gets re added (hashed to itself.) Then the second s hasn't been confirmed, so it also gets hashed (to the same hash ). Thus, we have duplicates in mSubstitutions

--- node.cpp
+++ node.cpp
@@ -28,14 +28,34 @@ vector<int> Node::substitutions(){
 void Node::appendSubstitution(int substitution) {
     mSubstitutions.push_back(substitution);
 }
-void Node::replaceSubstitutions(vector<int>substitutions){//** I know why the hash shifting happens in 2 parts, but it's kind of icky...
-    for (int i=0; i<mSubstitutions.size(); ++i){//go through existing substitutions. if an item is there but isn't in the new list, mark it as uncertain
-        if (find(substitutions.begin(), substitutions.end(), mSubstitutions[i])==substitutions.end()){
-            substitutions.push_back((mSubstitutions[i]/5)*5+4);
-            cout<<mSubstitutions[i]<<" substitution was found earlier but not present in this round---S, W or S, different S"<<endl;
+void Node::replaceSubstitutions(vector<int>currentSubstitutions){//check substitutions found in this read against those from other reads of this barcode. Non-matches are listed as unconfirmed
+    cout<<mSubstitutions.size()<<" this is the number of substitutuiosn listed"<<endl;
+    if (mCount>0){
+        vector <int> confirmedSubstitutions;
+        for (int i=0; i<mSubstitutions.size(); ++i){//go through existing substitutions. if an item is there but isn't in the new list, mark it as uncertain
+            int substitutionHash;
+            vector<int>::iterator pos = find(currentSubstitutions.begin(), currentSubstitutions.end(), mSubstitutions[i]);
+            if (pos == currentSubstitutions.end() ){
+                substitutionHash = (mSubstitutions[i]/5)*5+4;
+                cout<<substitutionHash<< "is being added to confirmedSubstitutions just got edited"<<endl;
+            }
+            else {
+                substitutionHash = mSubstitutions[i];
+                cout<<substitutionHash<< "is being added to confirmedSubstitutions about to be edited"<<endl;
+                currentSubstitutions.erase(pos);
+            }
+            confirmedSubstitutions.push_back(substitutionHash);
         }
+        for (int i=0; i<currentSubstitutions.size(); ++i){//hashes remaining in currentSubstitutions are unconfirmed
+            confirmedSubstitutions.push_back( (currentSubstitutions[i]/5)*5+4 );
+            cout<<((currentSubstitutions[i]/5 )* 5+4)<< " is being added to confirmedSubstitutions as an edited hash"<<endl;        
+        }
+        mSubstitutions = confirmedSubstitutions;//replace existing substitutions with confirmed substitutions
     }
-    mSubstitutions = substitutions;
+    else {//if this is the first read, no check is necessary
+        cout<<currentSubstitutions.front()<<" gets an automatic pass since it's the first read"<<endl;
+        mSubstitutions = currentSubstitutions;
+    }
 }
 
 bool Node::hasIndel(){
