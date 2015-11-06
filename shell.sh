f69464102d479a8c947ba5fac5a6a4ea5e4d3b8d
SusInMotion <steinman.tutoring@gmail.com>
2015-11-06 16:34:17 -0500

    Makefile

--- shell.sh
+++ shell.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-head -n 1 config.cfg>tmp.cfg
+head -n 2 config.cfg>tmp.cfg
 source tmp.cfg
 echo $FILENAMES
 arr=$(echo $FILENAMES | tr "," "\n")
 echo ${arr[*]}
-gzcat ${arr[*]} | ./secondtrie
+if [ "$ZIPPED" = "yes" ]
+	then
+	echo $ZIPPED
+	gzcat ${arr[*]} | ./secondtrie
+fi
+else
+	${arr[*]} | ./secondtrie
 rm tmp.cfg
