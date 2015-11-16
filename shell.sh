d81259603fa8961a043d113d6aaba1d9cb5640a5
SusInMotion <steinman.tutoring@gmail.com>
2015-11-16 14:13:23 -0500

    debugging multiple files read in without newline

--- shell.sh
+++ shell.sh
@@ -2,12 +2,10 @@
 
 head -n 2 configforveryshort5.cfg>tmp.cfg
 source tmp.cfg
-echo $FILENAMES
 arr=$(echo $FILENAMES | tr "," "\n")
 echo ${arr[*]}
 if [ "$ZIPPED" = "yes" ]
 	then
-	echo $ZIPPED
 	zcat ${arr[*]} | ./secondtrie
 
 else
