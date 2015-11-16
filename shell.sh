700f6f9314246244977dab16298db56a4e5c8db5
SusInMotion <steinman.tutoring@gmail.com>
2015-11-16 14:55:31 -0500

    adding new line between files

--- shell.sh
+++ shell.sh
@@ -4,11 +4,25 @@ head -n 2 configforveryshort5.cfg>tmp.cfg
 source tmp.cfg
 arr=$(echo $FILENAMES | tr "," "\n")
 echo ${arr[*]}
+
+
 if [ "$ZIPPED" = "yes" ]
 	then
-	zcat ${arr[*]} | ./secondtrie
+	arr2=
+	for f in ${arr[*]}
+    do
+    	arr2+=($f)
+    	arr2+=(trashzip.gz)
+    done
+    echo ${arr2[*]}
+	gunzip -c  ${arr2[*]} | ./secondtrie
 
 else
-        cat ${arr[*]} | ./secondtrie
+	for f in ${arr[*]}
+    do
+        echo "Processing $f"
+        sed -i '' -e '$a\' $f
+    done
+    cat ${arr[*]} | ./secondtrie
 fi
 rm tmp.cfg
